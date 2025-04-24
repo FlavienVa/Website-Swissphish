e10a65eee31c859685f58600f4396766c6712023
Flav <flavien.valea@epfl.ch>
2025-04-24 19:24:02 +0100

    just want to pull

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Home from "./pages/Home/Home";
+import Contact from "./pages/Contact";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 
@@ -10,9 +11,8 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" default element={<Home />} />
-        <Route path="/home" default element={<Home />}>
-          {" "}
-        </Route>
+        <Route path="/home" default element={<Home />} />
+        <Route path="/contact" element={<Contact />} />{" "}
       </Routes>
     </Router>
   );
